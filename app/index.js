0f3527e6afab0600d1836f9681b63da9e77780fd
Daavidmp <cod.davidmp@gmail.com>
2026-02-11 15:25:05 +0100

    despliegue

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('prueba\n');
+    res.end('Examen DAW Despliegue inicial”\n');
 });
 server.listen(3000, () => {
     console.log('Servidor corriendo en puerto 3000');
